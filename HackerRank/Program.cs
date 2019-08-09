d929022a54344005a3eca90d65a7ecf2caa22b3a
Brad <bjkapteyn@gmail.com>
2019-08-09 16:24:03 -0400

    First commit, right side ladder solution

---
+++ HackerRank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RightLadder.staircase(6);
+            Console.ReadKey();
+        }
+        
+    }
+    class Test
+    {
+
+    }
+}
---
+++ HackerRank/RightLadder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class RightLadder
+    {
+        public static void staircase(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                string s = "";
+                s += addChar(n - i, " ");
+                s += addChar(i, "#");
+                Console.WriteLine(s);
+            }
+
+        }
+        private static string addChar(int spaces, string character)
+        {
+            string result = "";
+            for(int i = 0; i < spaces; i++)
+            {
+                result += character;
+            }
+            return result;
+        }
+        
+    }
+}
