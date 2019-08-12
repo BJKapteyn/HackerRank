4c8ec23d9b6e2ceba8b3345e538a0b742115d8a5
Brad <bjkapteyn@gmail.com>
2019-08-12 13:58:12 -0400

    Added initial solution

--- HackerRank/ConvertToMilitaryTime.cs
+++ HackerRank/ConvertToMilitaryTime.cs
@@ -8,5 +8,33 @@ namespace HackerRank
 {
     class ConvertToMilitaryTime
     {
+        public string ConvertToMilitary(string s)
+        {
+            string result;
+            string[] timeElements = s.Split(':');
+            char[] remove = { 'M', 'A', 'P' };
+            if (AmOrPm(s))
+            {
+                if(timeElements[0] == "12")
+                {
+                    timeElements[0] = "00";
+                }
+                else
+                {
+                    timeElements[0] = (int.Parse(timeElements[0]) + 12).ToString();
+                }
+            }
+            result = String.Join(":", timeElements).Trim(remove);
+
+            return result;
+        }
+        public bool AmOrPm(string s)
+        {
+            if (s.ToUpper().Contains("PM"))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
