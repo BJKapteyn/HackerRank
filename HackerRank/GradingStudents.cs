3f7ebb9a9850d02e468e92794beee55d562b1099
Brad <bjkapteyn@gmail.com>
2019-08-12 17:39:44 -0400

    Added tests and initial solution

--- HackerRank/GradingStudents.cs
+++ HackerRank/GradingStudents.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace HackerRank
 {
-    class GradingStudents
+    public class GradingStudents
     {
+        public static int GradeStudent(int grade)
+        {
+            if(grade >= 40)
+            {
+                while(grade % 5 != 0)
+                {
+                    grade++;
+                }
+            }
+            return grade;
+        }
+
+        public static List<int> GradeAllStudents(List<int> grades)
+        {
+            foreach(int grade in grades)
+            {
+                GradeStudent(grade);
+            }
+            return grades;
+        }
+
+        [Theory]
+        [InlineData(40, 40)]
+        [InlineData(38, 38)]
+        [InlineData(44, 45)]
+        [InlineData(78, 80)]
+        public void GradeStudentTest(int test, int expected)
+        {
+            Assert.Equal(expected, GradeStudent(test));
+        }
+
     }
 }
