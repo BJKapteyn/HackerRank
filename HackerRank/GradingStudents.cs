aec619c380d12750a43107dbefb9651c5358da9e
Brad <bjkapteyn@gmail.com>
2019-08-12 20:42:09 -0400

    Final solution all tests passed

--- HackerRank/GradingStudents.cs
+++ HackerRank/GradingStudents.cs
@@ -11,7 +11,7 @@ namespace HackerRank
     {
         public static int GradeStudent(int grade)
         {
-            if(grade >= 40)
+            if(grade >= 38 && !((grade + 3) % 5 == 0 || (grade + 4) % 5 == 0) && grade != 0)
             {
                 while(grade % 5 != 0)
                 {
@@ -23,21 +23,44 @@ namespace HackerRank
 
         public static List<int> GradeAllStudents(List<int> grades)
         {
-            foreach(int grade in grades)
+            for(int i = 0; i < grades.Count; i++)
             {
-                GradeStudent(grade);
+                grades[i] = GradeStudent(grades[i]);
             }
             return grades;
         }
 
         [Theory]
-        [InlineData(40, 40)]
-        [InlineData(38, 38)]
-        [InlineData(44, 45)]
+        [InlineData(37, 37)]
+        [InlineData(38, 40)]
+        [InlineData(42, 42)]
         [InlineData(78, 80)]
+        [InlineData(61, 61)]
         public void GradeStudentTest(int test, int expected)
         {
             Assert.Equal(expected, GradeStudent(test));
+        }
+
+        [Fact]
+        public void GradeAllStuentsTest()
+        {
+            List<int> test = new List<int> { 32, 34, 69, 82 };
+            List<int> expected = new List<int> { 32, 34, 70, 82 };
+
+            Assert.Equal(expected, GradeAllStudents(test));
+        }
+        [Fact]
+        public void GradeAllStuentsTest2()
+        {
+            List<int> test = new List<int> {
+                84, 94, 21, 0, 18, 100, 18, 62, 30, 61, 53, 0, 43, 2, 29, 53, 61, 40
+            };
+            List<int> expected = new List<int> {
+                85, 95, 21, 0, 18, 100, 18, 62, 30, 61, 55, 0, 45, 2, 29, 55, 61, 40
+            };
+
+
+            Assert.Equal(expected, GradeAllStudents(test));
         }
 
     }
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -11,7 +11,28 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ConvertToMilitaryTime.ConvertToMilitary("07:45:19PM"));
+            List<int> test = new List<int> {
+                84, 94, 21, 0, 18, 100, 18, 62, 30, 61, 53, 0, 43, 2, 29, 53, 61, 40
+            };
+            List<int> expected = new List<int> {
+                85, 95, 21, 0, 18, 100, 18, 62, 30, 61, 55, 0, 45, 2, 29, 55, 61, 40
+            };
+            Console.WriteLine("\nTest numbers:");
+            foreach (int g in test)
+            {
+                Console.Write(g + " ");
+            }
+            List<int> grade = GradingStudents.GradeAllStudents(test);
+            Console.WriteLine("\nmine");
+            foreach(int g in grade)
+            {
+                Console.Write(g + " ");
+            }
+            Console.WriteLine("\nExpected");
+            foreach (int g in expected)
+            {
+                Console.Write(g + " ");
+            }
             Console.ReadKey();
         }
         
