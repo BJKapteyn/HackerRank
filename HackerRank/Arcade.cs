be333cc508c77cd525bbd509b95d5b9e3696afbb
Brad <bjkapteyn@gmail.com>
2019-08-13 14:16:48 -0400

    Refactored solution, completes most tests, can't handle large numbers, must refactor

--- HackerRank/Arcade.cs
+++ HackerRank/Arcade.cs
@@ -12,15 +12,15 @@ namespace HackerRank
         //alice is playing an arcade game, array one contains the scores of the game currently and the second is alices scores.
         //find where she is positioned within the high scores and return an array of each of the placements.
 
-        public static List<int> Leaderboard(int[] leaderboard, int[] aliceScores)
+        public static long[] Leaderboard(long[] leaderboard, long[] aliceScores)
         {
-            List<int> lbPacements = new int[leaderboard.Length + 1].ToList();
-            List<int> aPlacements = new int[aliceScores.Length].ToList();
-            List<int> tempLB = leaderboard.ToList();
+            List<long> lbPacements = new long[leaderboard.Length + 1].ToList();
+            long[] aPlacements = new long[aliceScores.Length];
+            List<long> tempLB = leaderboard.ToList();
             
-            for(int i = 0; i < aliceScores.Length; i++)
+            for(long i = 0; i < aliceScores.Length; i++)
             {
-                int count = 1;
+                long count = 1;
                 tempLB.Add(aliceScores[i]);
                 tempLB.Sort();
                 tempLB.Reverse();
@@ -30,8 +30,11 @@ namespace HackerRank
                 lbPacements[0] = count;
                 for(int k = 1; k < lbPacements.Count; k++)
                 {
-                   lbPacements[k] = lbPacements[k] ==  lbPacements[k - 1]?
-                                    count : ++count;
+                    if (tempLB[k] != tempLB[k - 1])
+                    {
+                        count++;
+                    }
+                    lbPacements[k] = count;
                 }
                 aPlacements[i] = lbPacements[alicePostion];
                 tempLB.RemoveAt(alicePostion);
@@ -45,10 +48,21 @@ namespace HackerRank
         [Fact]
         public void ArcadeTest1()
         {
-            List<int> expected = new List<int>{ 2, 4, 4, 2 };
-            int[] test = { 60, 10, 20, 50 };
-            int[] leaderboard = { 62, 50, 25, 10 };
-            List<int> actual = Arcade.Leaderboard(leaderboard, test);
+            long[] expected = { 2, 4, 4, 2 };
+            long[] test = { 60, 10, 20, 50 };
+            long[] leaderboard = { 62, 50, 25, 10 };
+            long[] actual = Arcade.Leaderboard(leaderboard, test);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ArcadeTest2()
+        {
+            long[] expected = { 6, 5, 4, 2, 1 };
+            long[] test = { 50, 65, 77, 90, 102 };
+            long[] leaderboard = { 100, 90, 90, 80, 75, 60 };
+            long[] actual = Arcade.Leaderboard(leaderboard, test);
 
             Assert.Equal(expected, actual);
         }
