0e5c232b7dcae3a4f52e4c377b67f3e123874ab1
Brad <bjkapteyn@gmail.com>
2019-08-20 14:52:13 -0400

    added breaking records method and test fail case

--- HackerRank/BreakingRecords.cs
+++ HackerRank/BreakingRecords.cs
@@ -9,10 +9,25 @@ namespace HackerRank
 {
     class BreakingRecords
     {
+        public static int[] breakingRecords(int[] scores)
+        {
+            int[] result = new int[2];
+
+            return result;
+
+        }
     }
 
     public class BreakingRecordsTest
     {
+        [Fact]
+        public void BreakingRecordsTest1()
+        {
+            int[] test = { 3, 4, 21, 36, 10, 28, 35, 5, 24, 42 };
 
+            int[] expected = { 4, 0 };
+            int[] actual = BreakingRecords.breakingRecords(test);
+
+        }
     }
 }
