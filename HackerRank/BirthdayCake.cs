0ffba140f346b825011372af3fbf2b956a5afe06
Brad <bjkapteyn@gmail.com>
2019-08-27 18:59:00 -0400

    Added failcase for test

--- HackerRank/BirthdayCake.cs
+++ HackerRank/BirthdayCake.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace HackerRank
 {
     class BirthdayCake
     {
+        //input list of numbers with 
+        public static int Birthday(List<int> s, int day, int month)
+        {
+            int result = 0;
+
+
+            return result;
+
+        }
+    }
+
+    public class BirthdayCakeTest
+    {
+        [Fact]
+
+        public void BirthdayCakeTest1()
+        {
+            List<int> test = new List<int> { 1, 2, 1, 3, 2, };
+            int expected = 0;
+            int actual = BirthdayCake.Birthday(test, 3, 2);
+
+            Assert.Equal(expected, actual);
+           
+        }
+
     }
 }
