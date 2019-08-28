12448877effeb5d87c6dea363cb5f1eef8b28c93
Brad <bjkapteyn@gmail.com>
2019-08-27 20:38:32 -0400

    Initial solution 1 test passed

--- HackerRank/BirthdayCake.cs
+++ HackerRank/BirthdayCake.cs
@@ -13,7 +13,19 @@ namespace HackerRank
         public static int Birthday(List<int> s, int day, int month)
         {
             int result = 0;
-
+            int total = 0;
+            for (int k = 0; k < s.Count - month; k++)
+            {
+                for (int i = k; i < k + month; i++)
+                {
+                    total += s[i];
+                }
+                if (total == day)
+                {
+                    result++;
+                }
+                total = 0;
+            }
 
             return result;
 
@@ -23,16 +35,23 @@ namespace HackerRank
     public class BirthdayCakeTest
     {
         [Fact]
-
         public void BirthdayCakeTest1()
         {
-            List<int> test = new List<int> { 1, 2, 1, 3, 2, };
+            List<int> test = new List<int> { 1, 2, 1, 3, 2 };
+            int expected = 2;
+            int actual = BirthdayCake.Birthday(test, 3, 2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BirthdayCakeTest2()
+        {
+            List<int> test = new List<int> { 1, 1, 1, 1, 1, 1 };
             int expected = 0;
             int actual = BirthdayCake.Birthday(test, 3, 2);
 
             Assert.Equal(expected, actual);
-           
         }
-
     }
 }
