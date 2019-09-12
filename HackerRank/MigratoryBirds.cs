5154aa92e10b6c266bdab3a1d9b3d39f2044fbba
Brad <bjkapteyn@gmail.com>
2019-09-12 09:29:40 -0400

    added test failcase

--- HackerRank/MigratoryBirds.cs
+++ HackerRank/MigratoryBirds.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace HackerRank
 {
     class MigratoryBirds
     {
+        public static int MigBirds(int[] birds)
+        {
+            int result = 0;
+
+            return result;
+        }
+    }
+
+    public class MigratoryBirdsTest
+    {
+        [Fact]
+        public static void MigTest1()
+        {
+            int[] test = { 1, 4, 4, 4, 5, 3 };
+            int expected = 4;
+            int actual = MigratoryBirds.MigBirds(test);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
