80e9b51025dd0476ae0204ababa8b499a5ea3c0c
Brad <bjkapteyn@gmail.com>
2019-09-21 11:53:07 -0400

    removed duplicate index check because each individual pair can be calculated even if the numbers are the same, both tests passed

--- HackerRank/DivisibleSum.cs
+++ HackerRank/DivisibleSum.cs
@@ -16,16 +16,16 @@ namespace HackerRank
 
             for(int i = 0; i < ar.Length; i++)
             {
-                if(i > 0 && ar[i] == ar[i-1])
-                {
-                    continue;
-                }
+                //if (i > 0 && ar[i] == ar[i - 1])
+                //{
+                //    continue;
+                //}
                 for (int x = i + 1; x < ar.Length; x++)
                 {
-                    if(ar[x] == ar[x -1])
-                    {
-                        continue;
-                    }
+                    //if(ar[x] == ar[x -1])
+                    //{
+                    //    continue;
+                    //}
                     if((ar[i] + ar[x]) % k == 0)
                     {
                         result++;
