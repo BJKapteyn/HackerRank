3652f220277040f94eac0dd311586d373fda2792
Brad <bjkapteyn@gmail.com>
2019-09-23 10:31:41 -0400

    added initial solution and test

--- HackerRank/DayOfProgrammer.cs
+++ HackerRank/DayOfProgrammer.cs
@@ -9,10 +9,43 @@ namespace HackerRank
 {
     class DayOfProgrammer
     {
+        public static string dayOfProgrammer(int year)
+        {
+            string result = "";
+            if(year < 1919)
+            {
+                if(year % 4 == 0)
+                {
+                    result = "12.09." + year;
+                }
+            }
+            else if(year >= 1919)
+            {
+                if(year % 4 == 0 || year % 400 == 0)
+                {
+                    result = "12.09." + year;
+                }
+            }
+            else
+            {
+                result = "13.09." + year;
+            }
+
+            return result;
+
+        }
     }
 
     public class DayOfProgrammerTest
     {
+        [Theory] 
+        [InlineData(2017, "13.09.2017")]
+        public void DOPTest1(int year, string expected)
+        {
+            int test = 2017;
+            string actual = DayOfProgrammer.dayOfProgrammer(year);
 
+            Assert.Equal(expected, actual);
+        }
     }
 }
