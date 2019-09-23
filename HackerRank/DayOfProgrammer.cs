6f12e828c87efb4530b01476ea23b1f2bb38c0e0
Brad <bjkapteyn@gmail.com>
2019-09-23 10:57:01 -0400

    added modulus 100 for post 1919 years

--- HackerRank/DayOfProgrammer.cs
+++ HackerRank/DayOfProgrammer.cs
@@ -20,7 +20,7 @@ namespace HackerRank
             }
             else if(year >= 1919)
             {
-                if(year % 4 == 0 || year % 400 == 0)
+                if((year % 4 == 0 && year % 100 != 0)|| year % 400 == 0)
                 {
                     return "12.09." + year;
                 }
@@ -32,9 +32,11 @@ namespace HackerRank
     public class DayOfProgrammerTest
     {
         [Theory] 
+        [InlineData(2100, "13.09.2100")]
         [InlineData(2017, "13.09.2017")]
         [InlineData(1984, "12.09.1984")]
         [InlineData(1800, "12.09.1800")]
+        [InlineData(1918, "26.09.1918")]
         public void DOPTest1(int year, string expected)
         {
             string actual = DayOfProgrammer.dayOfProgrammer(year);
