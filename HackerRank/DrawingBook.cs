3dc32d95adf49dc1e36c233dd2c7b93b545537c4
Brad <bjkapteyn@gmail.com>
2019-09-25 15:19:48 -0400

    added initial solution and extra tests, all tests passed

--- HackerRank/DrawingBook.cs
+++ HackerRank/DrawingBook.cs
@@ -10,10 +10,28 @@ namespace HackerRank
     //find the lowest amount of page turns for a book with p pages and a page number of n
     class DrawingBook
     {
-        public static int pageCount(int n, int p)
+        public static int pageCount(int n, int p = 0)
         {
             int result = 0;
+            int fromFront = 0;
+            int fromBack = 0;
+            if(n == p)
+            {
+                return 0;
+            }
 
+
+            fromFront = n / 2;
+
+            if(p % 2 == 0)
+            {
+                p++;
+            }
+
+            fromBack = (p - n) / 2;
+            
+            List<int> frontAndBack = new List<int>() { fromFront, fromBack };
+            result = frontAndBack.Min();
             return result;
         }
     }
@@ -21,7 +39,8 @@ namespace HackerRank
     public class DrawingBookTest
     {
         [Theory]
-        [InlineData (6, 2, 1)]
+        [InlineData (2, 6, 1)]
+        [InlineData(4, 5, 0)]
         public void DrawingBookTest1(int n, int p, int expected)
         {
             int actual = DrawingBook.pageCount(n, p);
