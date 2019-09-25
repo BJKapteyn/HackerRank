edae0ae3ad1fe6ed4d200c55ab1bf5f82860a510
Brad <bjkapteyn@gmail.com>
2019-09-25 14:46:14 -0400

    added pageCount method and fail test case

--- HackerRank/DrawingBook.cs
+++ HackerRank/DrawingBook.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace HackerRank
 {
+    //find the lowest amount of page turns for a book with p pages and a page number of n
     class DrawingBook
     {
+        public static int pageCount(int n, int p)
+        {
+            int result = 0;
+
+            return result;
+        }
+    }
+
+    public class DrawingBookTest
+    {
+        [Theory]
+        [InlineData (6, 2, 1)]
+        public void DrawingBookTest1(int n, int p, int expected)
+        {
+            int actual = DrawingBook.pageCount(n, p);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
