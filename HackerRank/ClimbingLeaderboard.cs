9ffb3bed79b3a501ac4cba38cd2cbcf856991178
Brad <bjkapteyn@gmail.com>
2019-09-28 12:22:14 -0400

    added two tests both fail

--- HackerRank/ClimbingLeaderboard.cs
+++ HackerRank/ClimbingLeaderboard.cs
@@ -7,21 +7,21 @@ using Xunit;
 
 namespace HackerRank
 {
-    //input, array of leaderboard scores and an array of Alice's
+    //input, array of leaderboard scores and an array of Alice's placements on the leaderboard, duplicate scores share placement number
     class ClimbingLeaderboard
     {
         public static int[] climbingLeaderboard(long[] scores, long[] alice)
         {
             int[] scorePlacements = new int[alice.Length];
             List<long> scoresList = scores.ToList();
-            //create scoreboard and remove duplicates
-            List<Player> playerScoreboard = CreateScoreBoard(scores).Distinct().ToList();
+            //create scoreboard and remove duplicates and order the list
+            List<Player> playerScoreboard = CreateScoreBoard(scores).Distinct().OrderByDescending(x => x.Score).ToList();
 
             for(int i = 0; i < alice.Length; i++)
             {
                 Player Alice = new Player("Alice", alice[i]);
                 playerScoreboard.Add(Alice);
-                playerScoreboard.OrderBy(x => x.Score);
+                playerScoreboard.OrderByDescending(x => x.Score);
                 int scoreIndex = playerScoreboard.IndexOf(Alice);
                 //if alice's score ties another score, add that placement instead, placement starts at 1, 2, 3 etc so actual placement is 
                 //index + 1;
@@ -33,6 +33,7 @@ namespace HackerRank
                 {
                     scorePlacements[i] = scoreIndex + 1;
                 }
+                playerScoreboard.Remove(Alice);
             }
 
             return scorePlacements.ToArray();
@@ -68,6 +69,13 @@ namespace HackerRank
     public class ClimbingLeaderboardTest
     {
         [Theory] 
-        public void ClimbingLeaderboardTest1()
+        [InlineData(new long[] {100, 100, 50, 40, 40, 20, 10 }, new long[] {5, 25, 50, 120 }, new int[] {6, 4, 2, 1 })]
+        [InlineData(new long[] { 100, 90, 90, 80, 75, 60 }, new long[] {50, 65, 77, 90, 102 }, new int[] { 6,5, 4, 2, 1})]
+        public void ClimbingLeaderboardTest1(long[] scores, long[] alice, int[] expected)
+        {
+            int[] actual = ClimbingLeaderboard.climbingLeaderboard(scores, alice);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
