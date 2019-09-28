3d46f6f7d29d7dcaddbb4adfd6f951f70fde2033
Brad <bjkapteyn@gmail.com>
2019-09-28 11:19:11 -0400

    added ClimbingLeaderboard and Player class for challenge

---
+++ HackerRank/ClimbingLeaderboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class ClimbingLeaderboard
+    {
+    }
+    class Player
+    {
+        public string Name { get; set; } = "OtherPlayer";
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+        public Player(int score)
+        {
+            Score = score;
+        }
+    }
+}
