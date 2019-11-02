70a4324d6284f85f1de2d7bb425afef3a3a94f52
Brad <bjkapteyn@gmail.com>
2019-11-02 12:55:48 -0400

    added class and test class for matching pairs challenge

---
+++ HackerRank/MatchingPairs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class MatchingPairs
+    {
+    }
+
+    public class MatchingPairsTest
+    {
+
+    }
+}
