538aafa873ac870a394dd3c0a7f340e312282115
Brad <bjkapteyn@gmail.com>
2019-11-10 18:45:28 -0500

    added class for hourglass challenge

---
+++ HackerRank/HourGlass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackerRank
+{
+    //calculate the values of an hourglass shape in a 6X6 2D array
+    class HourGlass
+    {
+    }
+
+    public class HourGlassTest
+    {
+
+    }
+}
