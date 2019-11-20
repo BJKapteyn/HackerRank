0539d291237c757bf59dd1dfab3c7a89ef924671
Brad <bjkapteyn@gmail.com>
2019-11-20 11:35:35 -0500

    inital solution all tests passed

--- HackerRank/Class1.cs
+++ HackerRank/Class1.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace HackerRank
 {
-    class Class1
+    //take an array and move the elements 'rotate' indexes to the left
+    class RotateArray
     {
+        public static int[] rotateArray(int[] arr, int rotate)
+        {
+            int[] result = new int[arr.Length];
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int rotatedIndex;
+                if((i - rotate) < 0)
+                {
+                    //if the index after the rotate operation is less than zero, continue the rotation from the end of the list
+                    rotatedIndex = arr.Length - Math.Abs(i - rotate);
+                }
+                else
+                {
+                    rotatedIndex = i - rotate;
+                }
+                result[rotatedIndex] = arr[i];
+            }
+
+            return result;
+        }
+    }
+
+    public class RotateArrayTest
+    {
+        [Theory]
+        [InlineData(new int[] { 5, 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4, 5 }, 4)]
+        public void rotateArrayTest1(int[] expected, int[] test, int rotate)
+        {
+            int[] actual = RotateArray.rotateArray(test, rotate);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
