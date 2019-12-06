9bf459310528eb3dc763691fc682502556eb3888
Brad <bjkapteyn@gmail.com>
2019-12-06 12:58:35 -0500

    added classes for my own single linked list

--- HackerRank/LinkedListNode.cs
+++ HackerRank/LinkedListNode.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace HackerRank
 {
-    class LinkedListNode
+   
+        
+    class SinglyLinkedListNode
     {
+        public int? data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+        {
+
+            SinglyLinkedListNode result = new SinglyLinkedListNode(data);
+            SinglyLinkedListNode current = head;
+            if(current == null)
+            {
+                this.head = result;
+            }
+            else
+            {
+
+                while(current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = result;
+            }
+
+            return result;
+
+        }
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+        }
+
+    }
+    
+
+    public class LinkedListNodeTest
+    {
+
     }
 }
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -11,19 +11,13 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            AppleTree atree = new AppleTree();
-            int s = 7;
-            int t = 11;
-            int a = 5;
-            int b = 15;
-            int[] apples = { -2, 2, 1 };
-            int[] oranges = { 5, -6 };
-
-            int[] expected = { 1, 1 };
-            int[] actual = atree.countApplesAndOranges(s, t, a, b, apples, oranges);
-
-            Assert.Equal(expected, actual);
-            Console.ReadKey();
+            SinglyLinkedList L = new SinglyLinkedList();
+            L.insertNodeAtTail(L.head, 14);
+            L.insertNodeAtTail(L.head, 15);
+            L.insertNodeAtTail(L.head, 16);
+            L.insertNodeAtTail(L.head, 17);
+            L.insertNodeAtTail(L.head, 15);
+            Console.WriteLine(L.head.next);
         }
         
     }
