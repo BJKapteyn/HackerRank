004d9c0be30fb92da5f510face9d4284bac463df
Brad <bjkapteyn@gmail.com>
2020-03-11 11:55:54 -0400

    wrote two tests fail check good

--- HackerRank/PickingNumbers.cs
+++ HackerRank/PickingNumbers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace HackerRank
 {
@@ -11,11 +12,29 @@ namespace HackerRank
     //absolute difference between any two of the chosen integers is less than or equal to 1
     class PickingNumbers
     {
+        public static int pickingNumbers(int[] integerArray)
+        {
+            int result = 0;
+
+            Array.Sort(integerArray);
+
+
+
+            return result;
+        }
     }
 
     public class PickingNumbersTest
     {
+        [Theory] 
+        [InlineData(new int[] { 1, 1, 2, 2, 4, 4, 5, 5, 5 }, 5)]
+        [InlineData(new int[] { 1, 2, 2, 3, 1, 2 }, 5)]
+        public void PickingNumbersTest1(int[] arr, int expected)
+        {
+            int actual = PickingNumbers.pickingNumbers(arr);
 
+            Assert.Equal(expected, actual);
+        }
     }
 
 }
