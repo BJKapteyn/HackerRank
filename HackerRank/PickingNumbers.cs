3a4fdb1f3c8f774658eb66a58e70a96e4a8ca7f4
Brad <bjkapteyn@gmail.com>
2020-03-11 11:44:32 -0400

    added class and test for picking numbers challenge

---
+++ HackerRank/PickingNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    //https://www.hackerrank.com/challenges/picking-numbers/problem
+    //Given an array of integers, find and print the maximum number of integers you can select from the array such that the 
+    //absolute difference between any two of the chosen integers is less than or equal to 1
+    class PickingNumbers
+    {
+    }
+
+    public class PickingNumbersTest
+    {
+
+    }
+
+}
