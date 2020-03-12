e6b41d1f5fdf9a30bba828d483f22badaae27bd5
Brad <bjkapteyn@gmail.com>
2020-03-12 12:23:10 -0400

    added method identifier and test, fail test good

--- HackerRank/HurdleRace.cs
+++ HackerRank/HurdleRace.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace HackerRank
 {
+    //https://www.hackerrank.com/challenges/the-hurdle-race/problem
+    //Dan is playing a video game in which his character competes in a hurdle race.Hurdles are of varying heights, 
+    //and Dan has a maximum height he can jump.There is a magic potion he can take that will increase his maximum height
+    //by unit 1 for each dose.How many doses of the potion must he take to be able to jump all of the hurdles.
     class HurdleRace
     {
+        public static int hurdleRace(int jumpHeight, int[] hurdleHeights)
+        {
+
+
+        }
+    }
+
+    public class HurdleRaceTest
+    {
+        [Fact] 
+        public void HurdleRaceTest1()
+        {
+            int jumpHeight = 7;
+            int[] hurdleHeights = { 2, 5, 4, 5, 2};
+
+            int expected = 0;
+            int actual = HurdleRace.hurdleRace(jumpHeight, hurdleHeights);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
