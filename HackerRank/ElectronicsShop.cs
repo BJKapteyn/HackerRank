3f6059f7d242394df8c5ba6f7bd85fd2a43e0c57
Brad <bjkapteyn@gmail.com>
2020-03-16 16:46:49 -0400

    added Electronics Shop challenge class and test class

---
+++ HackerRank/ElectronicsShop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackerRank
+{
+    https://www.hackerrank.com/challenges/electronics-shop/problem
+    class ElectronicsShop
+    {
+
+    }
+    public class ElectronicsShopTest
+    {
+
+    }
+}
