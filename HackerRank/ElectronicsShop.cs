b7b19f3aab58af7712b90a5fa28c94e1fcb34fc5
Brad <bjkapteyn@gmail.com>
2020-03-16 17:02:52 -0400

    wrote tests, fail case good

--- HackerRank/ElectronicsShop.cs
+++ HackerRank/ElectronicsShop.cs
@@ -7,13 +7,26 @@ using Xunit;
 
 namespace HackerRank
 {
-    https://www.hackerrank.com/challenges/electronics-shop/problem
+    //https://www.hackerrank.com/challenges/electronics-shop/problem
     class ElectronicsShop
     {
+        public static int getMoneySpent(int[] keyboards, int[] drives, int money)
+        {
+            int result = -1;
 
+            return result;
+        }
     }
     public class ElectronicsShopTest
     {
+        [Theory]
+        [InlineData(new int[] { 3, 1 }, new int[] { 5, 2, 8 }, 10, 9)]
+        [InlineData(new int[] { 4 }, new int[] { 5}, 5, -1)]
+        public static void ElectronicsShopTest1(int[] keyboards, int[] drives, int money, int expected)
+        {
+            int actual = ElectronicsShop.getMoneySpent(keyboards, drives, money);
 
+            Assert.Equal(expected, actual);
+        }
     }
 }
